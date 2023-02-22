bb4c3ff83703c1e824491716871dc27126b58626
aryar <arya.rizky4@gmail.com>
2023-02-22 15:56:22 +0700

    Generate CRUD Golang with sqlc and write unit test

--- util/random.go
+++ util/random.go
@@ -12,10 +12,12 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-func RandomInit (min, max int64) int64 {
+// generates a random integer between min and max
+func RandomInt (min, max int64) int64 {
 	return min + rand.Int63n(max - min + 1) 
 }
 
+// generates a random string of length n
 func RandomString (n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
@@ -28,3 +30,19 @@ func RandomString (n int) string {
 	return sb.String()
 }
 
+// generates random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+//generates random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+//generates random currency
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
