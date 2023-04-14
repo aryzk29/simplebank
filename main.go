8cf6e66e17d14a0c9f3488af065789040b793308
aryar <arya.rizky4@gmail.com>
2023-04-14 15:43:31 +0700

    implement HTTP api

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/aryzk29/simplebankcp/api"
+	db "github.com/aryzk29/simplebankcp/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	address  = "0.0.0.0:8080"
+)
+
+func main() {
+
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(address)
+	if err != nil {
+		log.Fatal("Cannot Start Server: ", err)
+	}
+
+}
