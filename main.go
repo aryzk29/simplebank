36c21bb8d6a66e03d9afc14c402bad320a608241
aryar <arya.rizky4@gmail.com>
2023-04-18 14:44:22 +0700

    add load config & env file with viper

--- main.go
+++ main.go
@@ -6,18 +6,17 @@ import (
 
 	"github.com/aryzk29/simplebankcp/api"
 	db "github.com/aryzk29/simplebankcp/db/sqlc"
+	"github.com/aryzk29/simplebankcp/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	address  = "0.0.0.0:8080"
-)
-
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(address)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot Start Server: ", err)
 	}
